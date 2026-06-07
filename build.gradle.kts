029d436e52f0c3ee5d437b56d07ad35a8529ceb8
sevouz <sevouz@users.noreply.github.com>
2026-06-07 23:41:46 +0530

    Fix build: replace broken -SNAPSHOT gradle plugin with pinned commit 81b1d424d

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream.gradle:gradle:81b1d424d")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
