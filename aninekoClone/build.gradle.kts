2add4726d74a4378665b6aee7a3b25585b3c9077
sevouz <sevouz@users.noreply.github.com>
2026-06-08 00:00:02 +0530

    Add aninekoClone plugin ported from phisher98/Anineko

---
+++ aninekoClone/build.gradle.kts
@@ -0,0 +1,26 @@
+// use an integer for version numbers
+version = 1
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+cloudstream {
+    description = "Anime from Anineko"
+    authors = listOf("Phisher98")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/Anineko.png"
+    isCrossPlatform = true
+}
