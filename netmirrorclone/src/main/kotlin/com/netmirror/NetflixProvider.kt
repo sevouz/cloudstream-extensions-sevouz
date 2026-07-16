e76b4146b356b951da9f9817877fb94eaa5d98b0
sevouz <sevouz@users.noreply.github.com>
2026-07-16 18:24:28 +0530

    change language to English, fix episode image URLs with correct prefixes per provider

--- netmirrorclone/src/main/kotlin/com/netmirror/NetflixProvider.kt
+++ netmirrorclone/src/main/kotlin/com/netmirror/NetflixProvider.kt
@@ -4,6 +4,7 @@ class NetflixProvider : BaseNetMirrorProvider() {
     override var name = "Netflix"
     override val ott = "nf"
     override val imgPrefix = "poster"
+    override val epImgPrefix = "epimg/150"
     override val searchPath = "search.php"
     override val postPath = "post.php"
     override val episodesPath = "episodes.php"
